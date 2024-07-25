63dc52c533d3a004da625a9c94b79457065b56c8
Saad Shoaib <binshoaibsaad@gmail.com>
2024-07-26 02:56:26 +0500

    frameloop

--- 3D-Portfolio/src/components/canvas/Ball.jsx
+++ 3D-Portfolio/src/components/canvas/Ball.jsx
@@ -30,7 +30,7 @@ const BallCanvas = ({ icon }) => {
   const isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
 
   return (
-    <Canvas frameLoop="demand" gl={{ preserveDrawingBuffer: true }}>
+    <Canvas frameloop="demand" gl={{ preserveDrawingBuffer: true }}>
       <Suspense fallback={<CanvasLoader />}>
         <OrbitControls enableZoom={false} />
         <Ball imgUrl={icon} />
