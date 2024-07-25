100cd8b3fef778caa62b9900321fd21b8ce1c488
Saad Shoaib <binshoaibsaad@gmail.com>
2024-07-26 02:48:58 +0500

    back to old

--- 3D-Portfolio/src/components/canvas/Ball.jsx
+++ 3D-Portfolio/src/components/canvas/Ball.jsx
@@ -1,10 +1,10 @@
-// Ball.jsx
-import React from 'react';
-import { Decal, Float, useTexture } from '@react-three/drei';
+import React, { Suspense } from 'react';
+import { Canvas } from '@react-three/fiber';
+import { Decal, Float, OrbitControls, Preload, useTexture } from '@react-three/drei';
+import CanvasLoader from '../Loader';
 
-const Ball = ({ imgUrl }) => {
-  const [decal] = useTexture([imgUrl]);
-
+const Ball = (props) => {
+  const [decal] = useTexture([props.imgUrl]);
   return (
     <Float speed={1.75} rotationIntensity={1} floatIntensity={2}>
       <ambientLight intensity={0.25} />
@@ -25,4 +25,21 @@ const Ball = ({ imgUrl }) => {
   );
 };
 
-export default Ball;
+const BallCanvas = ({ icon }) => {
+  // Assuming you want to handle mobile devices, define isMobile
+  const isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
+
+  return (
+    <Canvas frameLoop="demand" gl={{ preserveDrawingBuffer: true }}>
+      <Suspense fallback={<CanvasLoader />}>
+        <OrbitControls enableZoom={false} />
+        <Ball imgUrl={icon} />
+        {/* Remove or define the Computers component if necessary */}
+        {/* <Computers isMobile={isMobile} /> */}
+      </Suspense>
+      <Preload all />
+    </Canvas>
+  );
+};
+
+export default BallCanvas;
